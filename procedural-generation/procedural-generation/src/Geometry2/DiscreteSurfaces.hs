46075b6c5c243438dce79b08ed524dbb065fa341
marc <marc.rasi@gmail.com>
2016-12-03 13:26:23 -0800

    stuff

--- procedural-generation/procedural-generation/src/Geometry2/DiscreteSurfaces.hs
+++ procedural-generation/procedural-generation/src/Geometry2/DiscreteSurfaces.hs
@@ -17,13 +17,13 @@ cube = DiscreteSurface
         , Vertex $ V3 1 1 1
         , Vertex $ V3 0 1 1
         ]
-    , faces = fromList $
+    , faces = fromList (
         mkFaces 0 1 2 3 ++
         mkFaces 2 5 6 3 ++
         mkFaces 4 5 6 7 ++
         mkFaces 0 3 7 4 ++
         mkFaces 2 3 6 7 ++
-        mkFaces 0 1 5 4
+        mkFaces 0 1 5 4)
     }
 where
     mkFaces a b c d = [Face a b c, Face a c d]
